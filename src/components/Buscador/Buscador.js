0c14d316730d09ad8388c6a4f90c9b1f2e9167a7
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-03-23 18:41:50 -0300

    Responsiveness

--- src/components/Buscador/Buscador.js
+++ src/components/Buscador/Buscador.js
@@ -187,8 +187,41 @@ export default function Buscador({allPokemon}){
       <div className={s.background} style={{backgroundImage: `url(${background})`}}/>
       <div className={s.pokedex}>
         <img alt="pokedex" className={s.pokedexImg} src={pokedex}/>
-          <input className={s.input} onChange={handleInput} name='title' value={state.title} placeholder="Search by name or ID..." ></input>
-          <button type="submit" className={s.btnSubmit} onClick={handleSubmit} ></button>
+        <input className={s.input} onChange={handleInput} name='title' value={state.title} placeholder="Search by name or ID..." ></input>
+        <button type="submit" className={s.btnSubmit} onClick={handleSubmit} ></button>
+        <div className={s.info} data-tip data-for='tooltip'/>
+        <ReactTooltip id='tooltip' place='top' effect="solid" >
+          Welcome to Post-Modern Pokedex! <br/>
+          • You can use your keyboard's arrows or WASD to navigate.<br/>
+          • If you are using a mobile device, use the DPAD to navigate.<br/>
+          • Use A or Space to get details about any pokemon and B or the 'B' key to return<br/>
+          to the browser.<br/>
+          • You can also search pokemon by id or name.
+        </ReactTooltip>
+        <Route exact path='/' >
+          <Cards selected={selected.id} render={render}/>
+            {isMobile && 
+            <div className={s.dpad}>
+              <div>
+                <button onClick={handleUp} className={s.up} ></button>
+              </div>
+              <div>
+                <button onClick={handleLeft} className={s.left}  ></button>
+              <button onClick={handleRight} className={s.right}></button>
+              </div>
+              <div>
+              <button onClick={handleDown} className={s.down}></button>
+              </div>
+            </div>
+          }
+         <button onClick={handleA} className={s.a}></button>
+         <button className={s.b}></button>
+       </Route>
+       {/* <Route path='/pokemon' >
+         <Pokemon pokemon={pokemon} />
+         <button className={s.a}></button>
+         <button onClick={handleB} className={s.b}></button>
+       </Route> */}
       </div>
     </div>
   )
