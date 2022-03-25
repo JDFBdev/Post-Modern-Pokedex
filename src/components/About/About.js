c8569489875aadd62ccebcd4c565d412528ffcfe
J. Diego Fernández <JDFBdeveloper@gmail.com>
2022-03-24 23:52:11 -0300

    Added link to portfolio

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,6 +3,7 @@ import s from './About.module.css';
 import git from '../../Images/LOGO_GITHUB.png';
 import gmail from '../../Images/LOGO_GMAIL.png';
 import linkedin from '../../Images/LOGO_LINKEDIN.png';
+import JDFB from '../../Images/JDFB.png';
 import { useHistory } from "react-router-dom";
 
 export default function About({name, id, selected}) {
@@ -26,6 +27,7 @@ export default function About({name, id, selected}) {
             <img alt='linkedin Icon' src={linkedin} className={s.icon} onClick={() => { window.open('https://www.linkedin.com/in/jdfbdev/','_blank')}} />
             <img alt='Gmail Icon' src={gmail} className={s.icon} onClick={() => { window.open('mailto:JDFBdeveloper@gmail.com?','_blank')}} />
             <img alt='Git Icon' src={git} className={s.icon} onClick={() => { window.open('https://github.com/JDFBdev','_blank')}} />
+            <img alt='JDFB Icon' src={JDFB} className={s.icon} onClick={() => { window.open('https://jdfb-portfolio.vercel.app/','_blank')}} />
         </div>
         <button className={s.a}/>
         <button onClick={handleB} className={s.b}/>
